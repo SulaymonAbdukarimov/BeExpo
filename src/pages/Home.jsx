061187251a6c23c610f7e83c6a303c95eccab61b
SulaymonAbdukarimov <sulaymonabdukarimov2@gmail.com>
2022-05-11 03:12:10 +0500

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./styles/home.css";
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <>
@@ -74,6 +76,7 @@ function Home() {
           </button>
         </div>
       </div>
+      {/* /hero section */}
 
       {/* Products */}
       <div className="home_products ">
@@ -86,7 +89,7 @@ function Home() {
               <h4>
                 <a
                   href="#"
-                  className="text-decoration-none text-center d-block fw-bold fs-2 text-capitalize text-white"
+                  className="home-product--link text-decoration-none text-center d-block fw-bold fs-2 text-capitalize "
                 >
                   Best Have
                 </a>
@@ -98,7 +101,7 @@ function Home() {
               </span>
               <h4>
                 <a
-                  className="text-decoration-none text-center d-block fw-bold fs-2 text-capitalize text-white"
+                  className="home-product--link text-decoration-none text-center d-block fw-bold fs-2 text-capitalize "
                   href="#"
                 >
                   best sellers!
@@ -112,12 +115,148 @@ function Home() {
               <h4>
                 <a
                   href="#"
-                  className="text-center d-block text-decoration-none fw-bold fs-2 text-capitalize text-white"
+                  className=" home-product--link text-center d-block text-decoration-none fw-bold fs-2 text-capitalize "
                 >
                   keep bees
                 </a>
               </h4>
             </div>
+          </div>
+        </div>
+      </div>
+      {/* /Product section */}
+
+      {/* Action */}
+      <div className="home_action">
+        <div className="container">
+          <h2 className="home_action--title text-center mb-4">What we do</h2>
+          <div className="home-action__wrapper d-flex align-items-lg-center justify-content-between text-center gap-5">
+            <div className="home-action__honey">
+              <img
+                className="img-fluid"
+                src="https://beelove.ancorathemes.com/wp-content/uploads/2016/06/q1-2.png"
+                alt=""
+              />
+              <h4 className="mb-3 mt-4">
+                <a className="text-decoration-none" href="#">
+                  Beekeper Honey
+                </a>
+              </h4>
+              <p>
+                Professional beekeepers take agreat care of the farm and monitor
+                all processes to gather honey in time.
+              </p>
+            </div>
+
+            <div className="home-action__garden">
+              <img
+                className="img-fluid"
+                src="https://beelove.ancorathemes.com/wp-content/uploads/2016/06/q2-2.png"
+                alt=""
+              />
+              <h4 className="mb-3 mt-4">
+                <a className="text-decoration-none" href="#">
+                  Bee Garden
+                </a>
+              </h4>
+              <p>
+                We have built a tru cosy garden where our hard working bees
+                produce all sorts of local honey and nectars.
+              </p>
+            </div>
+
+            <div className="home-action__products">
+              <img
+                className="img-fluid"
+                src="https://beelove.ancorathemes.com/wp-content/uploads/2016/06/q3-2.png"
+                alt=""
+              />
+              <h4 className="mb-3 mt-4">
+                <a className="text-decoration-none" href="#">
+                  Bee Products
+                </a>
+              </h4>
+              <p>
+                Our products are 100% organic, produced on a farm with natural
+                ingredients addition only. Sweet & delicious.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/*//  Action */}
+
+      {/* Home Honey */}
+      <div className="home_honey">
+        <div className="container">
+          <h3 className="home_honey-title">
+            Our Honey Is Never Cooked, It Is Always Raw
+          </h3>
+          <h5 className="home_honey-subtitle">
+            TO PRESERVE FLAVOR AND HEALTH BENEFITS
+          </h5>
+
+          <Link
+            className="home_link d-block d-flex justify-content-center align-items-center text-center text-uppercase fw-normal text-decoration-none text-white fs-5"
+            to="/honey"
+          >
+            Our Honey
+          </Link>
+        </div>
+      </div>
+
+      {/* Home Honey */}
+
+      {/* Blog */}
+
+      <div className="home-blog">
+        <div className="container">
+          <h2 className="text-center h2 home-blog_title">Blog</h2>
+          <div className="home-blog_wrapper d-flex justify-content-between align-items-center gap-4">
+            <div className="home-blog-item">
+              <img
+                src="https://beelove.ancorathemes.com/wp-content/uploads/2016/06/post-7-370x204.jpg"
+                alt="bee"
+              />
+              <span className="d-block text-center">June 9, 2016</span>
+              <a href="#" className="text-decoration-none d-block text-center">
+                Tutorial: Beekeeping Plans, Supplies & Ideas
+              </a>
+            </div>
+            <div className="home-blog-item">
+              <img
+                src="https://beelove.ancorathemes.com/wp-content/uploads/2016/06/post-8-370x204.jpg"
+                alt="bee"
+              />
+              <span className="d-block text-center">June 6, 2016</span>
+              <a className="text-decoration-none d-block text-center" href="#">
+                Spring Break for Millions of Bees and Their Keepers
+              </a>
+            </div>
+            <div className="home-blog-item">
+              <img
+                src="https://beelove.ancorathemes.com/wp-content/uploads/2016/06/post-2-370x204.jpg"
+                alt=""
+              />
+              <span className="d-block text-center">June 3, 2016</span>
+              <a className="text-decoration-none d-block text-center" href="#">
+                Tutorial: Beekeeping for Beginners. Breeding Bees
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Learn about honey  */}
+      <div className="honey-learn">
+        <div className="container">
+          <h3 className="text-center honey-learn-title">Learn about Honey</h3>
+          <div className="honey-learn-wrapper">
+            <div className="honey-learn-item honey-learn-item1"></div>
+            <div className="honey-learn-item honey-learn-item2"></div>
+            <div className="honey-learn-item honey-learn-item3"></div>
+            <div className="honey-learn-item honey-learn-item4"></div>
+            <div className="honey-learn-item honey-learn-item5"></div>
+            <div className="honey-learn-item honey-learn-item6"></div>
           </div>
         </div>
       </div>
